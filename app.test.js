dfe520edfe679075f27c88f1c3bb0ef9efdfcc50
Naman Attri <namanattri5@gmail.com>
2018-11-29 00:33:09 +0530

    mongoose installation and jest configuration for mongoose.

--- app.test.js
+++ app.test.js
@@ -2,8 +2,12 @@
  * Test Server
  */
 const express = require('express')
+const mongoose = require('mongoose')
+
 const app = express()
 const port = 3001
+
+mongoose.connect('mongodb://localhost:27017/shopXpress_test')
 
 const routes = require('./routes/main');
 app.use("/", routes);
