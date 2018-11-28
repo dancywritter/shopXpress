2c4aa37353456be12f808ee01ee442c55a96b1bf
Naman Attri <namanattri5@gmail.com>
2018-11-28 15:59:52 +0530

    Make code changes and pass the method not allowed test case on route.

--- app.test.js
+++ app.test.js
@@ -2,7 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3001
 
-app.get('/', (req, res) => res.json({status: true, message: "shopXpress an expressJS based RESTful ecommerce engine."}));
+app.all('/', (req, res) => {
+  if(req.method == 'GET') {
+    res.json({status: true, message: "shopXpress an expressJS based RESTful ecommerce engine."})
+  } else {
+    res.status(405).json({status: false, message: "Method "+req.method.toUpperCase()+" not allowed on /"})
+  }
+});
 app.get('/hello', (req, res) => res.json({status:true, message: "Hello World!"}));
 
 module.exports = app;
