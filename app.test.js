5d75d92dc856571402d20706ef5bb8ec16a3ad0d
Naman Attri <namanattri5@gmail.com>
2018-11-28 22:49:55 +0530

    Isomorphic shared file based routes.

--- app.test.js
+++ app.test.js
@@ -1,14 +1,11 @@
+/**
+ * Test Server
+ */
 const express = require('express')
 const app = express()
 const port = 3001
 
-app.all('/', (req, res) => {
-  if(req.method == 'GET') {
-    res.json({status: true, message: "shopXpress an expressJS based RESTful ecommerce engine."})
-  } else {
-    res.status(405).json({status: false, message: "Method "+req.method.toUpperCase()+" not allowed on /"})
-  }
-});
-app.all('/hello', (req, res) => res.json({status:true, message: "Hello World!"}));
+const routes = require('./routes/main');
+app.use("/", routes);
 
 module.exports = app;
