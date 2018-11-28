db545d127a8d2d027406f94e4fb6ca142aa67eff
Naman Attri <namanattri5@gmail.com>
2018-11-28 14:52:06 +0530

    change app-test port to 3001

--- app.test.js
+++ app.test.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3001
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/hello', (req, res) => res.json({status:true, message: "Hello World!"}));
